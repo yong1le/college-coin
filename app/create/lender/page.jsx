a3da6d7fdc4ec70afae0a652fe95c3651cb4bbe1
yong1le <ygle.he@gmail.com>
2023-10-14 15:40:33 -0400

    update routes

--- app/create/lender/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreateLender = () => {
-  return (
-    <div>createLender</div>
-  )
-}
-
-export default createLender
--- app/home/lender/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LenderDashboard = () => {
-  return (
-    <div>LenderDashboard</div>
-  )
-}
-
-export default LenderDashboard
